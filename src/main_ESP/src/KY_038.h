c03cb1f51d5d4b9ded3a1bbb90d1c9c62c6a8b15
Carl3000P <caiopoppi@gmail.com>
2018-08-18 16:25:55 -0300

    Created a new file for KY-038 functions

---
+++ src/main_ESP/src/KY_038.h
@@ -0,0 +1,33 @@
+void setupKY_038(){
+  pinMode(ky038APin, INPUT);
+  pinMode(ky038DPin, INPUT);
+}
+
+void volumeHigh(){
+  if(!equipamentBlocked){
+    Serial.write("01");
+    digitalWrite(raspberryPin, HIGH);
+  }
+}
+
+void volumeLow(){
+  if(!equipamentBlocked){
+    Serial.write("00");
+    digitalWrite(raspberryPin, LOW);
+  }
+}
+
+void ky_read(void *args){
+  while(1){
+      ky038AValue = analogRead(ky038APin);
+      ky038DValue = digitalRead(ky038DPin);
+      vTaskDelay(50);
+      Serial.println(ky038AValue, DEC);
+      if(ky038AValue >= MAX_KY_VALUE){
+        volumeHigh();
+    } else  {
+        volumeLow();
+      }
+  }
+  vTaskDelete( NULL );
+}
