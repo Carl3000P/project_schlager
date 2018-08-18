811002c0032138acae191c1347c416fc2bc28acd
Carl3000P <caiopoppi@gmail.com>
2018-08-18 16:26:31 -0300

    Added control variables and pins to a new file

---
+++ src/main_ESP/src/MainController.h
@@ -0,0 +1,11 @@
+int ky038APin = A0;
+int ky038DPin = 15;
+
+int raspberryPin = 0; //TODO define raspberryPin
+int unoPin = 32;
+
+int ky038AValue = 0;
+int ky038DValue = 0;
+
+int MAX_KY_VALUE = 100;
+bool equipamentBlocked = true;
