3be46f94676a0ab642aefd380527750b863b07fc
Carl3000P <caiopoppi@gmail.com>
2018-10-06 19:40:14 -0300

    Hot fix for Ultrasonic code

--- src/main_ATMEGA/src/UltrasonicUtils.h
+++ src/main_ATMEGA/src/UltrasonicUtils.h
@@ -8,6 +8,8 @@
 Ultrasonic ultrasonic1(pino_trigger1, pino_echo1);
 Ultrasonic ultrasonic2(pino_trigger2, pino_echo2);
 
+bool stopProximity1 = false;
+bool stopProximity2 = false;
 bool stopProximity = false;
 float cmMsec1;
 long microsec1;
@@ -15,19 +17,26 @@ float cmMsec2;
 long microsec2;
 
 void ultrasonicRead(){
+
   microsec1 = ultrasonic1.timing();
   cmMsec1 = ultrasonic1.convert(microsec1, Ultrasonic::CM);
   if(cmMsec1 <= 30){
-    stopProximity = true;
+    stopProximity1 = true;
   } else {
-    stopProximity = false;
+    stopProximity1 = false;
   }
 
   microsec2 = ultrasonic2.timing();
   cmMsec2 = ultrasonic2.convert(microsec2, Ultrasonic::CM);
   if(cmMsec2 <= 30){
-    stopProximity = true;
+    stopProximity2 = true;
   } else{
-    stopProximity = false;
+    stopProximity2 = false;
+  }
+
+  if(stopProximity2 || stopProximity1){
+    stopProximity = true;
+  } else {
+      stopProximity = false;
   }
 }
